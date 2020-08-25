36a68b43440ade243079ba87b75df4515b510c0e
rishabhgupta <rishabhgupta@msn.com>
2020-08-25 21:55:42 +0530

    feat: update code component to show code by line

--- src/components/Code.svelte
+++ src/components/Code.svelte
@@ -1,22 +1,30 @@
 <script>
     export let syntax;
+
+    const lines = syntax.split("|");
 </script>
 
 <style>
     .code {
         overflow: auto;
         padding: 1.5rem 2rem;
-        margin: .8rem 0 2.4rem;
+        margin: 0.8em 0em;
         /* max-width: var(--code-w); */
         background-color: aliceblue;
         border-radius: var(--border-r);
         font-family: monospace;
         color: #666;
-        box-shadow: 1px 1px 1px rgba(68, 68, 68, .12) inset;
+        box-shadow: 1px 1px 1px rgba(68, 68, 68, 0.12) inset;
     }
 
+    p {
+        margin: 0;
+        padding: 0;
+    }
 </style>
 
-<div class="code"> 
-    {syntax}
+<div class="code">
+    {#each lines as line}
+        <p>{line}</p>
+    {/each}
 </div>
