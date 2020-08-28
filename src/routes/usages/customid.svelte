ea2632bbe1ac6ef5010ff038fed94117e63cf610
rishabhgupta <rishabhgupta@msn.com>
2020-08-28 18:46:58 +0530

    docs: ✏️ update custom id and remove docs

--- src/routes/usages/customid.svelte
+++ src/routes/usages/customid.svelte
@@ -1,15 +1,30 @@
 <script>
     import Sidebar from "../../components/Sidebar.svelte";
+    import Code from "../../components/Code.svelte";
+
+    const SNIPPETS = {
+        customId: `
+            toast.success(
+                "This is a success toast with id 121.",
+                { toastId: "121" }
+            );
+        `,
+    };
 </script>
 
 <svelte:head>
     <title>Custom ID</title>
 </svelte:head>
-<div class="page">
+<div class="usages-page">
     <Sidebar />
-    <section class="page__container">
-        <div class="content">
-            <h1>Custom ID</h1>
+    <section class="usages-page__container">
+        <div class="usages-content">
+            <h1>Use a custom id</h1>
+            <p>
+                by default each toast is assigned a unique id, you can pass a
+                custom id to replace it.
+            </p>
+            <Code syntax={SNIPPETS.customId} />
         </div>
     </section>
 </div>
--- src/routes/usages/remove.svelte
+++ src/routes/usages/remove.svelte
@@ -1,15 +1,59 @@
 <script>
     import Sidebar from "../../components/Sidebar.svelte";
+    import Code from "../../components/Code.svelte";
+    import { toast } from "svelte-toastify";
+
+    const SNIPPETS = {
+        id: `
+            const id = toast.success("This is a toast.");
+        `,
+        remove: `
+            toast.delete(id);
+        `,
+    };
+
+    let toastId;
+
+    const fireToast = () => {
+        toastId = toast.success(
+            "This is a toast which will not get removed untill the user clicks on the cross icon",
+            {
+                autoClose: false,
+            }
+        );
+    };
+
+    const deleteToast = () => {
+        toast.delete(toastId);
+    };
 </script>
 
 <svelte:head>
     <title>Delete Toast</title>
 </svelte:head>
-<div class="page">
+<div class="usages-page">
     <Sidebar />
-    <section class="page__container">
-        <div class="content">
-            <h1>Delete Toast</h1>
+    <section class="usages-page__container">
+        <div class="usages-content">
+            <h1>Remove toast programatically</h1>
+            <p>
+                every toast call returns a toast id. If custom toast id is
+                passed via
+                <code>toastId</code>
+                option then that will be retuned, else generated id is retuned.
+            </p>
+            <Code syntax={SNIPPETS.id} />
+            <p>
+                You can use this save this id and use it to programatically
+                remove a toast.
+            </p>
+            <Code syntax={SNIPPETS.remove} />
+            <button class="secondary" on:click={fireToast}>
+                Fire a Toast!
+            </button>
+            <button class="secondary" on:click={deleteToast}>
+                Delete the Toast!
+            </button>
         </div>
     </section>
 </div>
