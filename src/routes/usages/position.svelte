fed71a848af27768aacf5ee38c5407b602adfa42
rishabhgupta <rishabhgupta@msn.com>
2020-09-05 11:24:03 +0530

    docs: ✏️ adds position and style routes

--- src/routes/usages/position.svelte
+++ src/routes/usages/position.svelte
@@ -5,11 +5,18 @@
 <svelte:head>
     <title>Positioning</title>
 </svelte:head>
-<div class="page">
+<div class="usages-page">
     <Sidebar />
-    <section class="page__container">
-        <div class="content">
+    <section class="usages-page__container">
+        <div class="usages-content">
             <h1>Positioning</h1>
+            <!-- svelte-ignore a11y-no-onchange -->
+            By default, all the toasts will be positioned on the top right of
+            your browser. If a position is set on a toast, the one defined on
+            ToastContainer will be replaced. The following values are allowed:
+            top-right, top-center, top-left, bottom-right, bottom-center,
+            bottom-left For convenience, toast exposes a POSITION property to
+            avoid any typos.
         </div>
     </section>
 </div>
--- src/routes/usages/style.svelte
+++ src/routes/usages/style.svelte
@@ -5,10 +5,10 @@
 <svelte:head>
     <title>Styling</title>
 </svelte:head>
-<div class="page">
+<div class="usages-page">
     <Sidebar />
-    <section class="page__container">
-        <div class="content">
+    <section class="usages-page__container">
+        <div class="usages-content">
             <h1>Styling</h1>
         </div>
     </section>
